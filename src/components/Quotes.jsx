f3ac5b0e4563811176ffd4a7041bb942dfc8b113
Juan David <juand8012@gmail.com>
2021-05-13 22:47:12 -0500

    Hook for changing color was created

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useQuotesApi} from '../hooks/useQuotesApi.jsx';
-import {MainContainer} from '../styles/QuotesStyled.jsx';
+import {MainContainer, QuoteSection} from '../styles/QuotesStyled.jsx';
 
 const Quotes = () => {
     const quotes = useQuotesApi('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
@@ -8,10 +8,10 @@ const Quotes = () => {
     return (
         <div>
             <MainContainer>
-                <div>
-                    <h3>{`${data[0] && data[0].quote}`}</h3>
-                    <p>{`${data[0] && data[0].author}`}</p>
-                </div>
+                <QuoteSection>
+                    <h1 quote={`${data[0] && data[0].quote}`}></h1>
+                    <p author={`${data[0] && data[0].author}`}></p>
+                </QuoteSection>
             </MainContainer>     
         </div>
     )
--- src/styles/QuotesStyled.jsx
+++ src/styles/QuotesStyled.jsx
@@ -6,6 +6,20 @@ export const ChangeColor = [
   '#AC4818',
   '#0F471C',
   '#852D6D',
+  '#30EA25',
+  '#A2866C',
+  '#38240A',
+  '#EB5965',
+  '#991B52',
+  '#490ECE',
+  '#2A98D3',
+  '#23BA5C',
+  '#21230D',
+  '#AB7A7C',
+  '#4BADA4',
+  '#9AE9D6',
+  '#40A69A',
+  '#EAEC8C'
 ];
 
 export const MainContainer = styled.main`
@@ -13,4 +27,18 @@ export const MainContainer = styled.main`
     justify-content:center;
     align-items:center;
     height:100vh;
+    background-color: #A0C2DF;
 `
+
+export const QuoteSection = styled.main`
+    width: 28.75rem;
+    padding: 2.5rem 3.75rem;
+    border-radius: 1rem;
+    background-color: #FFFFFF;
+`
+
+const Quote = styled.h1`
+    
+`
+
+export const QuoteStyled = ({quoteParam}) => <Quote>{quoteParam}</Quote>
